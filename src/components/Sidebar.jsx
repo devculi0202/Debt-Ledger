abda2e101a9dd092b78e2eeade4ca7e0b7f6ee80
devculi <lethaiduy0202@gmail.com>
2026-07-29 10:30:09 +0700

    update text

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -58,7 +58,7 @@ export default function Sidebar({
           }`}
         >
           <Layers className="w-5 h-5 shrink-0" />
-          <span className="sidebar-text whitespace-nowrap">Master Debt</span>
+          <span className="sidebar-text whitespace-nowrap">Master Debts</span>
         </button>
 
         <button
